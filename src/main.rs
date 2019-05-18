3bfb3b4b84bdef7693218f32f5feddc9938539f3
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2019-05-17 23:47:49 -0700

    Adding hashmap containing original pairs mapping station names to abbreviation codes. The names need to be altered to match the natural language of the end-user.

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use alexa_sdk::{Request,Response};
 use alexa_sdk::request::{IntentType};
 use std::error::Error;
 use std::io::Read;
+use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use serde_json::{Result, Value};
-
+use std::hash::Hash;
 
 
 fn http_get(url: &str) -> reqwest::Result<String> {
@@ -83,6 +84,57 @@ fn handle_cancel(_req: &Request) -> std::result::Result<Response,HandlerError> {
 }
 
 fn handler(req: Request, _ctx: Context) -> std::result::Result<Response,HandlerError> {
+    let stations: HashMap<&str, &str> = [
+        ("12th St. Oakland City Center", "12th"),
+        ("16th St. Mission (SF)", "16th"),
+        ("19th St. Oakland", "19th"),
+        ("24th St. Mission (SF)", "24th"),
+        ("Ashby (Berkeley)", "ashb")
+        ("Antioch", "antc"),
+        ("Balboa Park (SF)", "balb"),
+        ("Bay Fair (San Leandro)", "bayf"),
+        ("Castro Valley", "cast"),
+        ("Civic Center (SF)", "civc"),
+        ("Coliseum", "cols"),
+        ("Colma", "colm"),
+        ("Concord", "conc"),
+        ("Daly City", "daly"),
+        ("Downtown Berkeley", "dbrk"),
+        ("Dublin/Pleasanton", "dubl"),
+        ("El Cerrito del Norte", "deln"),
+        ("El Cerrito Plaza", "plza"),
+        ("Embarcadero (SF)", "embr"),
+        ("Fremont", "frmt"),
+        ("Fruitvale (Oakland)", "ftvl"),
+        ("Glen Park (SF)", "glen"),
+        ("Hayward", "hayw"),
+        ("Lafayette", "lafy"),
+        ("Lake Merritt (Oakland)", "lake"),
+        ("MacArthur (Oakland)", "mcar"),
+        ("Millbrae", "mlbr"),
+        ("Montgomery St. (SF)", "mont"),
+        ("North Berkeley", "nbrk"),
+        ("North Concord/Martinez", "ncon"),
+        ("Oakland Int'l Airport", "oakl"),
+        ("Orinda", "orin"),
+        ("Pittsburg/Bay Point", "pitt"),
+        ("Pittsburg Center", "pctr"),
+        ("Pleasant Hill", "phil"),
+        ("Powell St. (SF)", "powl"),
+        ("Richmond", "rich"),
+        ("Rockridge (Oakland)", "rock"),
+        ("San Bruno", "sbrn"),
+        ("San Francisco Int'l Airport", "sfia"),
+        ("San Leandro", "sanl"),
+        ("South Hayward", "shay"),
+        ("South San Francisco", "ssan"),
+        ("Union City", "ucty"),
+        ("Warm Springs/South Fremont", "warm"),
+        ("Walnut Creek", "wcrk"),
+        ("West Dublin", "wdub"),
+        ("West Oakland", "woak")
+    ].iter().cloned().collect();
+
 
     match req.intent() {
         IntentType::Help => handle_help(&req),
