e858ee26308563d3790d67bc0090fce5b4389863
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2019-05-24 22:04:36 -0700

    Updating build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cargo build --release --target x86_64-unknown-linux-musl --features vendored
-zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap
+cargo build --release --target x86_64-unknown-linux-musl --features vendored && \
+zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap && \
 
-echo "updating function code"
+echo "updating function code" && \
 aws lambda update-function-code --function-name bart_info --no-publish --zip-file fileb://rust.zip --region us-west-2
