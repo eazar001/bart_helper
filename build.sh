6d7aba762f95e090840ecdd0a77b47e049a9d518
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2019-05-24 21:50:05 -0700

    Adding automated AWS function code upload to build script.

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 
 cargo build --release --target x86_64-unknown-linux-musl --features vendored
 zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap
+
+echo "updating function code"
+aws lambda update-function-code --function-name bart_info --no-publish --zip-file fileb://rust.zip --region us-west-2
