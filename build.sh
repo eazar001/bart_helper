024caaae29751f0da1b816b80b8474e9f2cc1d1d
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2019-05-26 18:44:47 -0700

    Adding development profile to build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
-cargo build --release --target x86_64-unknown-linux-musl --features vendored && \
-zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap && \
+if [[ -z $1 ]]; then
+    echo "Building development binary"
+    cargo build --target x86_64-unknown-linux-musl --features vendored && \
+    zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap && \
 
-echo "updating function code" && \
-aws lambda update-function-code --function-name bart_info --no-publish --zip-file fileb://rust.zip --region us-west-2
+    echo "updating function code" && \
+    aws lambda update-function-code --function-name bart_info --no-publish --zip-file fileb://rust.zip --region us-west-2
+    exit 0
+fi
+
+if [[ $1 == "release" ]]; then
+    echo "Building production binary"
+    cargo build --release --target x86_64-unknown-linux-musl --features vendored && \
+    zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap && \
+
+    echo "updating function code" && \
+    aws lambda update-function-code --function-name bart_info --no-publish --zip-file fileb://rust.zip --region us-west-2
+else
+    exit 1
+fi
