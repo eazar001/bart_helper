2068a7107da36b3061e33d866f1940bae0bff5ce
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2019-09-27 17:18:01 -0700

    Preparing v1.0.

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ if [[ -z $1 ]]; then
     zip -j rust.zip ./target/x86_64-unknown-linux-musl/debug/bootstrap && \
 
     echo "updating function code" && \
-    aws lambda update-function-code --function-name bart_info --no-publish --zip-file fileb://rust.zip --region us-west-2
+    aws s3 cp rust.zip s3://bart-helper/rust.zip && \
+    aws lambda update-function-code --function-name bart_info --no-publish --region us-west-2 --s3-bucket bart-helper --s3-key rust.zip
     exit 0
 fi
 
@@ -16,7 +17,8 @@ if [[ $1 == "release" ]]; then
     zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap && \
 
     echo "updating function code" && \
-    aws lambda update-function-code --function-name bart_info --no-publish --zip-file fileb://rust.zip --region us-west-2
+    aws s3 cp rust.zip s3://bart-helper/rust.zip && \
+    aws lambda update-function-code --function-name bart_info --publish --region us-west-2 --s3-bucket bart-helper --s3-key rust.zip
 else
     exit 1
 fi
